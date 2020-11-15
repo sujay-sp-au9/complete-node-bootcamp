c24bbc8ed5bd76eaa4cdc2523d718b44f49e79dd
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-11-15 21:05:28 +0530

    prettier setup

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -28,61 +28,79 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 // FILES
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
-const tempView = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const tempView = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
 
-const productData = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'));
+const productData = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+);
 
-const slugs = productData.map(element => slugify(element.productName, { lower: true, replacement: '_' }));
+const slugs = productData.map((element) =>
+  slugify(element.productName, { lower: true, replacement: '_' })
+);
 console.log(slugs);
 
 const server = http.createServer((req, res) => {
-    let { query, pathname } = url.parse(req.url, true);
-    query = JSON.parse(JSON.stringify(query));
-    switch(pathname){
-        // Overview page
-        case '/overview':
-        case '/':
-            res.writeHead(200, {
-                'Content-type': 'html'
-            });
+  let { query, pathname } = url.parse(req.url, true);
+  query = JSON.parse(JSON.stringify(query));
+  switch (pathname) {
+    // Overview page
+    case '/overview':
+    case '/':
+      res.writeHead(200, {
+        'Content-type': 'html',
+      });
 
-            const cardsHtml = productData.map(element => replaceTemplate(tempView, element)).join('');
-            const overviewOutput = tempOverview.replace('{%PRODUCT_CARDs%}', cardsHtml);
+      const cardsHtml = productData
+        .map((element) => replaceTemplate(tempView, element))
+        .join('');
+      const overviewOutput = tempOverview.replace(
+        '{%PRODUCT_CARDs%}',
+        cardsHtml
+      );
 
-            res.end(overviewOutput);
-            break;
+      res.end(overviewOutput);
+      break;
 
-        // Product page
-        case '/product':
-            res.writeHead(200, {
-                'Content-type': 'text/html'
-            });
-            const product = productData[query.id];
-            const productOutput = replaceTemplate(tempProduct, product);
-            res.end(productOutput);
-            break;
+    // Product page
+    case '/product':
+      res.writeHead(200, {
+        'Content-type': 'text/html',
+      });
+      const product = productData[query.id];
+      const productOutput = replaceTemplate(tempProduct, product);
+      res.end(productOutput);
+      break;
 
-        // API
-        case '/api':
-            res.writeHead(200, {
-                'Content-type': 'application/json'
-            });
-            res.end(JSON.stringify(productData));
-            break;
+    // API
+    case '/api':
+      res.writeHead(200, {
+        'Content-type': 'application/json',
+      });
+      res.end(JSON.stringify(productData));
+      break;
 
-        // NOT FOUND
-        default:
-            res.writeHead(404, {
-                'Content-type': 'text/html'
-            });
-            res.end('<h1><strong>Nice try</strong></h1>');
-    }
+    // NOT FOUND
+    default:
+      res.writeHead(404, {
+        'Content-type': 'text/html',
+      });
+      res.end('<h1><strong>Nice try</strong></h1>');
+  }
 });
 
 server.listen(3000, '127.0.0.1', () => {
-    console.log("Listening to request on port 3000");
+  console.log('Listening to request on port 3000');
 });
 
 // SERVER
