7b1f40e44fdbc1d02b4a9fd8e7a383bac087c3c5
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-11-16 14:46:50 +0530

    section

---
+++ 2-how-node-works/starter/event-loop.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 2;
+
+setTimeout(() => console.log("TIMER 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
+
+fs.readFile("test-file.txt", () => {
+  console.log("I/O finished");
+  setTimeout(() => console.log("TIMER 2 finished"), 0);
+  setTimeout(() => console.log("TIMER 3 finished"), 3000);
+  setImmediate(() => console.log("Immediate 2 finished"));
+
+  process.nextTick(() => console.log("Process.nextTick"));
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512", () =>
+    console.log(Date.now() - start + "\nPassword encrypted")
+  );
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () =>
+    console.log(Date.now() - start + "\nPassword encrypted")
+  );
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () =>
+    console.log(Date.now() - start + "\nPassword encrypted")
+  );
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () =>
+    console.log(Date.now() - start + "\nPassword encrypted")
+  );
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start + "\nPassword encrypted");
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start + "\nPassword encrypted");
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start + "\nPassword encrypted");
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start + "\nPassword encrypted");
+});
+
+console.log("Top level code");
