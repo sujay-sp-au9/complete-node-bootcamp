3a0e908856576b69f1684a622813169a2989ee1b
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-11-18 15:15:36 +0530

    section

---
+++ 3-asynchronous-JS/starter/index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject(err);
+      else resolve("Success");
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro("dog-img.txt", res.body.message);
+  })
+  .then(console.log("Random image link saved"))
+  .catch((err) => console.log(err));
