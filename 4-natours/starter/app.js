f8fef038910576516834e8533e8874f834a22fb6
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-11-20 16:16:57 +0530

    section

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,13 +1,68 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server', app: 'natours' });
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from the server', app: 'natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('POST!');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours/:id?', (req, res) => {
+  const id = req.params.id;
+  if (req.params.hasOwnProperty('id') && id) {
+    if (id >= tours.length) {
+      res.status(404).json({
+        status: 'fail',
+        message: 'invalid ID',
+      });
+    } else {
+      const tour = tours.find((element) => element.id == id);
+      res.status(200).json({
+        status: 'success',
+        data: {
+          tour,
+        },
+      });
+    }
+  } else {
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  }
 });
 
-app.post('/', (req, res) => {
-  res.send('POST!');
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) console.log(err);
+      else
+        res.status(201).json({
+          status: 201,
+          data: {
+            tour: newTour,
+          },
+        });
+    }
+  );
 });
 
 const port = 3000;
