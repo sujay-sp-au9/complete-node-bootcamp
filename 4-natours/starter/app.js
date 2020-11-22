22a5dad4f7452e0c591b410989a6f03dd593d4cc
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-11-22 21:54:36 +0530

    section6 complete

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,9 +8,11 @@ const app = express();
 
 /////////////// MIDDLEWARES
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
