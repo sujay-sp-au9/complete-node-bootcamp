e85c3befde9c50f30821a3067929066a82c2a745
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-11-25 00:48:49 +0530

    moved apiFeatures class to a file

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,52 +1,5 @@
 const Tour = require('../models/tourModel');
-
-class ApiFeatures {
-  constructor(query, queryStr) {
-    this.query = query;
-    this.queryStr = queryStr;
-  }
-
-  filter() {
-    const queryObj = { ...this.queryStr };
-    const excludeField = ['page', 'sort', 'limit', 'fields'];
-    excludeField.forEach((element) => delete queryObj[element]);
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = JSON.parse(
-      queryStr.replace(/\bgte|gt|lt|lte\b/, (match) => `$${match}`)
-    );
-    this.query.find(queryStr);
-    return this;
-  }
-
-  sort() {
-    if (this.queryStr.sort) {
-      const sortBy = this.queryStr.sort.split(',').join(' ');
-      this.query.sort(sortBy);
-    } else {
-      this.query.sort('-createdAt');
-    }
-    return this;
-  }
-
-  limitField() {
-    if (this.queryStr.fields) {
-      const fields = this.queryStr.fields.split(',').join(' ');
-      this.query.select(fields);
-    } else {
-      this.query.select('-__v');
-    }
-    return this;
-  }
-
-  pagination() {
-    const page = this.queryStr.page * 1 || 1;
-    const limit = this.queryStr.limit * 1 || 100;
-    const skip = limit * (page - 1);
-    this.query.skip(skip).limit(limit);
-    return this;
-  }
-}
+const ApiFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
