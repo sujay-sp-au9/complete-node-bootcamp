5645b9bb5dc12333acd6f4974fd951c056923f77
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-11-27 17:12:38 +0530

    Section 9 complete

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,3 +1,15 @@
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down');
+  process.exit(1);
+});
+
+process.on('unhandledException', (err) => {
+  console.log('UNHANDLED EXCEPTION! Shutting down');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -17,17 +29,10 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB connection yaay!'));
+  .then(() => console.log('DB connection yaay!'))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`App running on port ${port}`);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNHANDLED REJECTION! Shutting down');
-  server.close(() => {
-    process.exit(1);
-  });
 });
