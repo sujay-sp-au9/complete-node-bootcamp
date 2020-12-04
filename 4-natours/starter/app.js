fb51c39b6de856960c5195376fb6ce65d0abe289
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-12-04 13:53:03 +0530

    Added data sanitization middlewares

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -24,6 +26,11 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against noSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xssClean());
 
 /////////////// ROUTES
 
