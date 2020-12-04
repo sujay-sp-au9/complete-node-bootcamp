83b59f80515ea4ce61ede2c1bc59349e21e2f2cc
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-12-04 14:00:04 +0530

    Added http parameter pollution middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xssClean = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -31,6 +32,8 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xssClean());
+// Prevent parameter pollution
+app.use(hpp({ whitelist: ['duration'] }));
 
 /////////////// ROUTES
 
