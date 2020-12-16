69188363cf9a3aa5144c8f6a0cf98a72c10b39f2
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-12-16 19:05:38 +0530

    Logout completed

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -6,13 +6,6 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const sendEmail = require('../utils/email');
 
-const cookieOptions = {
-  expires: new Date(
-    Date.now + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-  ),
-  httpOnly: true,
-};
-
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -26,7 +19,13 @@ const createSendToken = (user, statusCode, res, options) => {
     user.password = undefined;
     response.data = { user };
   }
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = false;
+  const cookieOptions = {
+    expires: new Date(
+      Date.now + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
   res.cookie('jwt', token, cookieOptions);
   res.status(statusCode).json(response);
 };
@@ -77,6 +76,7 @@ exports.protect = catchAsync(async (req, res, next) => {
 //Only for rendered pages
 exports.isLoggedIn = catchAsync(async (req, res, next) => {
   if (req.cookies.jwt) {
+    if (req.cookies.jwt === 'loggedout') return next();
     const decoded = await promisify(jwt.verify)(
       req.cookies.jwt,
       process.env.JWT_SECRET
@@ -88,6 +88,16 @@ exports.isLoggedIn = catchAsync(async (req, res, next) => {
   }
   next();
 });
+
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    status: 'success',
+  });
+};
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
