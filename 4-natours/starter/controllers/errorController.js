40fb24f73500dd9b149fc433a35580892d5b70df
sujay-sp-au9 <sujay.sp19@gmail.com>
2020-12-16 20:00:52 +0530

    Error handler updated for rendered website

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,29 +1,49 @@
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-};
-
-const sendErrorProd = (err, res) => {
-  // Operational, trusted
-  if (err.isOperational) {
+const sendErrorDev = (err, res, req) => {
+  if (req.originalUrl.startsWith('/api')) {
     res.status(err.statusCode).json({
       status: err.status,
+      error: err,
       message: err.message,
+      stack: err.stack,
     });
   } else {
-    // Programming or other unknown error: dont leak error details
+    res.status(err.statusCode).render('error', {
+      title: 'Uh oh! Something went wrong!',
+      msg: err.message,
+    });
+  }
+};
 
-    // 1 log error
+const sendErrorProd = (err, res, req) => {
+  if (req.originalUrl.startsWith('/api')) {
+    // Operational, trusted
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    } else {
+      // Programming or other unknown error: dont leak error details
+
+      // 1 log error
+      console.error('ERROR: ', err);
+
+      // 2 send generic response
+      res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong',
+      });
+    }
+  } else if (err.isOperational) {
+    res.status(err.statusCode).render('error', {
+      title: 'Uh oh! Something went wrong!',
+      msg: err.message,
+    });
+  } else {
     console.error('ERROR: ', err);
-
-    // 2 send generic response
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went wrong',
+    res.status(err.statusCode).render('error', {
+      title: 'Uh oh! Something went wrong!',
+      msg: 'Please try again later',
     });
   }
 };
@@ -33,7 +53,7 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'fail';
   err.message = err.message || 'none';
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, res, req);
   } else if (process.env.NODE_ENV === 'production') {
     if (
       err.name === 'CastError' ||
@@ -47,7 +67,7 @@ module.exports = (err, req, res, next) => {
       err.isOperational = true;
       err.statusCode = 401;
     }
-    sendErrorProd(err, res);
+    sendErrorProd(err, res, req);
   }
   next();
 };
