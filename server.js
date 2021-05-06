eb44d96a8e0e752353df4ee4dc22c1ae83fa5721
sujay-sp-au9 <sujay.sp19@gmail.com>
2021-05-06 16:42:20 +0530

    final

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNHANDLED REJECTION! Shutting down');
-  process.exit(1);
-});
-
-process.on('unhandledException', (err) => {
-  console.log('UNHANDLED EXCEPTION! Shutting down');
-  console.log(err.name, err.message);
-  process.exit(1);
-});
-
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -33,6 +21,30 @@ mongoose
   .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('unhandledException', (err) => {
+  console.log('UNHANDLED EXCEPTION! Shutting down');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received! Shutting down');
+  server.close(() => {
+    process.exit(1);
+  });
 });
